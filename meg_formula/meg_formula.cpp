898985949b3ef9c4dad6c5131cfe8859a2697adb
00nuushi <naokitamemoto@DESKTOP-UH7681T.localdomain>
2020-06-02 03:59:42 +0900

    add meg_formula

---
+++ meg_formula/meg_formula.cpp
@@ -0,0 +1,443 @@
+/*
+はぴふり！(っ*⁰▿⁰)っ.｡:*+゜゜+*:.｡.*:+☆
+
+                                             `
+    `  `  `  `  ` ` `   `  `  ` ` `  `   .....____._. ` `  `  `   `  `  `  `  `  `   `  `  `   `
+  `      `            `            ` .._...............__...              `        `        `   `
+      `      `   `        `  `  ` ._.....`..`................_-  `  `  `       `       `  `      `
+ `  `     `   `    `  `       ` ._..`..`..`...`.`.`.`...`..`...-_    `    `  `  `   `         `    `
+   `   `        `   `   `  `  ._.`...`.....`...`.....-(<-...`....--     `               `   `
+         `   `            `  _.....`..`..`....`...--<!` <-....`...._.`        `  `   `          `
+  `   `     `    ` `   `    .....`._-..`..`..`..-(!`````.(-.`...`...-.   `  `      `    `  `       `
+       `            `      .-. _````_........-(<``````````__...`.....-.         `        `   ` `
+   `     `    `   `     ` _._``` .. ``-.`..-?~``````.`.``.`.<.....`...-.   `      `  `          `
+ `    `    `    `        _... ` ((:_```---!`````.`.``.`.```. <-`..`..`.-.`   `         `   `       `
+    `  `    `  `  `  `   ....``` _`` .(-!````.```.````.....```(-...`....-.    `  `  `   `    `  `
+   `      `         `   _....-.`.```.(<``.`.``.````.`._...  _ `-_...`....-.`      `
+      `      `   `    ` _..`........_`._`.__.``.``.`._```````..` _...`.`..-.   `     `   `  `  `
+ `     `       `   `    ....`....-~` _..___```.``.````````````````....`....._     `    `        ` `
+   `     `  `           .`...`.-_```.``.````.```.````.`.`..`.``.```_....`.`.._      `     `  `
+     `           `  `  ...`....-.``.`````.```.````.`.````````.```.`..`...`...._` `       `
+  `   `  `  `  `   `   _...`..`..````.``.``.``.``.````.`.``.``.`.``...`....`..._     `        `  `
+                       ..`..`.....`.``.```.````.```.``..._.JggJ,.``....`..`..`.-   `    `  `
+   `  `   `  `  ` `  `_.......`...``.`.``.`...``.``.````("7~_~?"~.`.`..`........_     `        `
+  `    `      `       _.`.`..`..`..``..gMMHB0_.```.``.``.````......`..`..`.`.`..-    `   `  `   `  `
+          `      `    ...`..`......` jY=.`.````.````.``.``.``...........`...`...- `       `  `
+    ` `      `    `   ........`.`..``....``.````.``..``````.````..`..`...`....`._     `        `  `
+ `     ` `  `  `    ` .`..`..`............`.`.```.` ``.`.`````````..`.`...`..`.-`  `   `   `
+   `             `    .`...`....`.`.....``.``.`.```````.``.`..`.``.....`...`...`    `    `    `   `
+      `               -..`....`....`.```````````.`..`.....`````.`._.`...`....-(.  `         `    `
+          `  `  `     _.....`...`.....`.`.`.``.`.JY>:::<=```.`. (:...`..`..`-;:;.    `  `      `
+  `  `   `        `    _.`.`..`..`..`...``.``.```_~~:~~``.`.` (<;<.`..`....(:;;;;_         `
+   `   `     `  `   `   `-...`....`....-<-..``.``````.````..(;;:;<..`....-;::;:;;;;-  `       `   `
+                         .--...`...`.`..(;;;<-. .`.`.``. .(;;;:;:<...`..(<;:;;::;:;;<.  `  `    `
+  `   `  `  `  `   `  `  -:<<-..`.....`..<<<;<::;;_---(;;+;;:;;:;<..`..(<;:;;:;;:;;:;;-  `   `
+                 `       ;:;::<<-.`..`...(<<z;:::;::::::<z>?+<;;;<-.....-<;;:;;:;;:;;;;~       `
+   `     `    `       ` (;;:;;;;;<<--...`.(<;:;:;:;::;:;;?>>>>+1+<<-`..`..(<;;:;:;;:;;:;<  `     `
+      `                ,;:;;:;<!~ ```(+-...<:;::;::;;::::;;>?>>><  _<-.....(;;:;;:;;:;:;;_    `
+ `       `  `  ` `  ` .;;<<!````````` <<...(;::;:;::;:;::;;?>>?>:````` _!--(<;;:;:;:;:;;;<  `     `
+   `      `          .!~``````` `` ``` >.`.(;;::;;:::;:;:::?>>>:```` `````.<~<;;;:;;:;:;;_      `
+     `              .?qa.``` ``` `` ``.<...>:;:;::;;::;:;:;+>?<`` `` ` `` ``` ((:;:;;:;:;! `  `
+      `  `   `  `   (dMMM,``` ``` ````(::--(+;;:;::;:;:;:;:z<!```` ```` ````.MN+;;:;:;;;;         `
+  `                 W@@@@@N,`` ``` ```` ~<:;;<<+;:;:::;;::+> `` ```` `````.J@@@Hs;;:;:;;<      `
+    `  `  `     ` `.@@@@@@@@h.`` ``  ` ``````.QJJJ+++++++g>`` `` `` `` ` .g@@@@@N;;;:;::<  `
+  `      `    `   .H@@@g@@@@@@N,.````````` ``` TMMMMMMMMM^```` `` ````.+@@@@@@@@@2:;;;;;;.   `  `
+      `          `(@@@@@@g@@@@@@@mJ,`` ` ```````(H@@@@@H^```` `````..W@@@@@@g@@@@b;::;:;;;_.   `
+   `            `.@@@@g@@@@@g@@@@@g@MMmJ...  ```` 4@@@H!` .-_``.-g@g@@@g@@@@@@g@@gx;;:;:;:;;-  ..  `
+         `  ` `  (@@@@@@@@@@@@g@@@@@@@@@@@@gHm-. ``?@H'  _..(...`.U@@@@@@g@@@@@@@@P;:;;:;;:;;;;!
+ `    `          W@@g@@g@g@@@@@g@@@@@@@@@@@@@@Nx__`.dD`.(_````?5.``?HM@@@@@g@@g@@@P!<<<:<<<!!`   `
+                .@@@@@@@@@g@g@@@@@@g@@g@@@@@@@HHN_ .`` .~(@H+ ``._ `,WWM@@@@@@@@g@]
+        `   `   -@@@g@@@@@@@@@@@@@@@@@@@g@@#"``.?^``._._.``.TW+.`````.vRH@@@g@@@@@F           `
+  `  `   `    ` d@@@@g@g@@@g@@@@@@g@@g@@HY!``.,!``.`._--~(,.`. !`.``.`(KW@@@@@g@@@P  `    `     `  `
+       `       .H@@@@@@@g@@@@@@@@@@@@@@@```._```._.`` .~``,Hm,````.``.`d@@@@@@@@@@P     `  `
+   `           ,@@g@@@@@@@@@M@@g@@g@@@@```.````_. `..`_.. `` ```.`````..H@@@g@@g@@N          `  `
+         `  ` .H@@@@g@@g@@g@M@@@@@@g@Y``````.`.``..<``` (.`_.````.`.````J@M@@@@@@@@_ ` `
+  `           .@@@@@@@@@@@@@MM@@g@@@F`.`.``.````..;<._`._(-``~.`````.``..MM@@@g@@@@]      `    `  `
+   `  `      .H@@@@g@@g@@g@@@MN@@@@@:````.```.````.` ._`(;;<.`_.`.```.```JM@@@@g@@@b   `    `
+        `  ` (@@@@@@@@@@@@@@@@MMNM@H_``.``.```.`..+>````.;>>;- _.`.```.``.v@@@@@@g@@,          `
+  `         .@@@@@g@@g@@@NNMMMMMMMMF``.````.``..<>;>_````(;>>>-`_..``.``.``vNM@@@@@@L   `        `
+         `  d@@@@@@@@MNMMMMMMMMMM5~````.`.``. .>;>;><`` ` >;;;>-`` &.`````.`JMMMMN@@N    ` ` `     `
+   `  `    .@@@@g@@@MMMMMMNMMB=`````.````.`._ .>;;>;;_`` `.;;>;;-``,N,.``.```.WMMMMN@[        `
+  `     `  J@@@@@@@MMMMMMB"`````.``.`.`...dL``.;>;>;;;.````<;>;;> ``dN-.```.```7MMMMNb    `      `
+          .@@@@@@@@MM#Y<_`````.``.````..d@@N ` ;;>;>;;<````.;;>>>_` dM]`.```.```/MMMMM;  `   `
+     `    d@@@@@@@@M3:::-`.`.``.``..JMM@@@@@h.`(;>;>>;;_` ` <;;;;_`.MM#.`.```.`.(<MMMMN
+ `     ` d@@@@@@@@@N<;:;< `.``.` .MMMMHMMMMNMN,`->;;>;><```.;>>>;_`.MMM+.`..``..::JMM@ML     `  ` `
+        .@@@@@@@@@@@h<;:;<-.``..MMMMMMM@@g@@@@Hx .>;;>;!`` <;;>;<`.dMMMR:_-.._::;:dMM@MH  `    `
+   `    .@@@@@g@@@@@@N<;::;:jgMMMMMNMMM@@@g@@@@Nx -;>>< `` `(>><`(dMMMMMNJ<:;:;;+uMM@@MN
+  `     ,@@@@@@g@@g@@@MNg&gMMMMMMMMMMM@@@@@@@@@@Nx. ~~`` - `` `` (MMMMMMMMMNNggMMM@@@@MM_   `     `
+       ` THHH@@@@@@@@@@@@MMMMMMMMMMMM@@@@@@g@@@@@@m. ` .+@e.``` (@@MNM@MMMMMMMM@@g@gMMMM]  `  `
+   `          ?TMMNM@NNMMMNNNMMMMMM@@@@@@g@@@g@@@@@Ne&gW@@@h.-(j@@@@MMMMMMNNMMMMMNNMMMMMF        `
+      `           .7WMMMMMMMMMM@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@g@@@@@@@MMMMMMMMMMMMMMB"!       `
+ `  `   `            .@@MMMM@@@@@@@@@@@@g@@g@@g@@g@@@@@@@g@@@@@@@@@@@g@MNMMMMM"=`        `     `  `
+         `  `       .M@@@@@g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@g@@@@g@@@@@@@@@@@@@[            `  `
+              `   ` -@@@@@@@@@@@@@@@g@@g@@g@@g@@g@@g@@g@@@@@@g@@@@g@@@@@@@@@@@@o    `  `
+   ` ` `  `     `   M@@@@@@@g@@@@@g@@@@@@@@@@@@@@@@@@@@g@g@@@@@@@@@g@@g@@@@g@@@@x `     `  `    `
+                   .M@@@@@g@@@@@@@@@@g@@g@@g@@g@@g@@g@@@@@g@@g@@g@@@@@@@g@@@@@@@@,           `    `
+            `     `d@@@@@@@@@g@@g@@g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@g@@@@@@@g@@@@H,  ` `
+ ` `  `  `    `  `.M@@@@g@@g@@@@@@@@@@g@@g@@g@@g@@g@@g@g@@g@@g@@g@@@@@g@@g@@@@g@@@N,      `  `  `
+                  .M@@@@@@@@@@g@@g@@g@@@@@@@@@@@@@@@@@@@@@@@@@@@@@g@@@@@@@@g@@@@@@@N.    `     `
+     `    `  `  ` J@@g@@g@@g@@@@@@@@@@@g@@g@@g@@g@@g@@g@@g@@g@@@@@@g@@@g@@@@@@@g@@@@h      `      `
+  `   `  `        Mg@@@@@@@@@@g@@g@@g@@@@@@@@@@@@@@@@@@@@@@@@g@@g@@@@g@@@g@@g@@@@@@@@L `     `
+                 .N@@@g@@g@@g@@@@@@@@@@g@@g@@g@@g@@g@@g@@g@@@@@@@@@@@@@@@@@@@@g@@g@@@@[  `     `  `
+   `        `  ` -M@@@@@@@@@@@@g@@g@@g@@@@@@@@@@@@@@@@@@@@g@@@g@@g@@g@@g@@g@@@@@@@g@@@@,   `
+     `  `        dM@@g@@g@@g@@@@@@@@@@@@g@@g@@g@@g@@g@@g@@@@@g@@@@@@@@@@@@@@g@@g@@@@@@@N.    `   `
+
+      The original image was downloaded from the tweet of Megu Shinonome (https://twitter.com/megu_shinonome/status/1254553909705637889/photo/1).
+      The ASCII art were created by using AA変換(アスキーアート生成) on the website (https://tool-taro.com/image_to_ascii/).
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <utility>
+
+#define DELTA 0.0001
+#define PI 3.14159265
+
+std::vector<int> get_meg_number();
+bool is_valid_input(std::string membership_number);
+bool get_meg_formula(std::string membership_number, std::vector<int> &meg_number, std::string &meg_formula);
+std::vector<std::vector<int> > get_all_brackets(std::vector<std::vector<int> > &left_brackets_index, std::vector<std::vector<int> > &right_brackets_index);
+bool is_valid_brackets(std::vector<int> &brackets, std::vector<std::pair<int, int> > &brackets_pairs);
+std::string make_formula(std::vector<int> &meg_number, std::vector<char> &operators, std::vector<int> &brackets,
+                         std::vector<std::vector<int> > &left_brackets_index, std::vector<std::vector<int> > &right_brackets_index);
+int fast_pow(int x, int n);
+double calc_formula(std::string meg_formula, int now, int &next);
+double calc(double left, char op, double right);
+
+int main()
+{
+  std::vector<int> meg_number;
+  std::string membership_number, meg_formula;
+  bool exist_meg_formula;
+
+  std::cout << "あなたの会員番号を入力してください" << std::endl;
+  std::cin >> membership_number;
+  meg_number = get_meg_number();
+  if (is_valid_input(membership_number) )
+  {
+    exist_meg_formula = get_meg_formula(membership_number, meg_number, meg_formula);
+    std::cout << std::endl;
+    if (exist_meg_formula)
+    {
+      std::cout << "あなたの会員番号を与えるめぐ式は" << meg_formula << "です" << std::endl;
+    }
+    else
+    {
+      std::cout << "残念ながらあなたの会員番号を与えるめぐ式は見つかりませんでした……" << std::endl;
+    }
+  }
+  else
+  {
+    std::cout << "入力が不正です。非負の半角数字のみを入力してください" << std::endl;
+  }
+}
+
+/*++++++++++ functions ++++++++++*/
+std::vector<int> get_meg_number()
+{
+  std::vector<int> ans;
+  ans.push_back(1);
+  ans.push_back(2);
+  ans.push_back(7);
+  ans.push_back(17);
+  ans.push_back(21);
+  ans.push_back(32);
+  return ans;
+}
+
+bool is_valid_input(std::string membership_number)
+{
+  int length = membership_number.size();
+  bool ans = true;
+
+  for (int i = 0; i < length; i++)
+  {
+    if (membership_number[i] < '0' || membership_number[i] > '9')
+    {
+      ans = false;
+      break;
+    }
+  }
+  return ans;
+}
+
+bool get_meg_formula(std::string membership_number, std::vector<int> &meg_number, std::string &meg_formula)
+{
+  /*
+  each bracket:
+  0 = (01)2345, 1 = (012)345, ... , 14 = 0123(45)
+  */
+  int number, max_operators, operator_digit, progress, count, dummy;
+  std::vector<char> operator_characters, operators(5);
+  std::vector<std::vector<int> > all_brackets;
+  std::vector<std::vector<int> > left_brackets_index(6), right_brackets_index(6);
+  bool can_get_meg_formula;
+
+  operator_characters.push_back('+');
+  operator_characters.push_back('-');
+  operator_characters.push_back('*');
+  operator_characters.push_back('/');
+  max_operators = fast_pow(4, meg_number.size()-1);
+  left_brackets_index.at(0) = {0, 1, 2, 3, 4};
+  left_brackets_index.at(1) = {5, 6, 7, 8};
+  left_brackets_index.at(2) = {9, 10, 11};
+  left_brackets_index.at(3) = {12, 13};
+  left_brackets_index.at(4) = {14};
+  right_brackets_index.at(1) = {0};
+  right_brackets_index.at(2) = {1, 5};
+  right_brackets_index.at(3) = {2, 6, 9};
+  right_brackets_index.at(4) = {3, 7, 10, 12};
+  right_brackets_index.at(5) = {4, 8, 11, 13, 14};
+  number = std::atoi(membership_number.c_str());
+  can_get_meg_formula = false;
+  all_brackets = get_all_brackets(left_brackets_index, right_brackets_index);
+  progress = 0;
+  count = 1;
+  do {
+    if (count >= 720 * (progress / 10.0)){
+      std::printf("\r計算中 φ(.. ) [");
+      for (int j = 0; j < progress; j++) std::printf("*");
+      for (int j = progress; j < 10; j++) std::printf(".");
+      std::printf("]");
+      std::fflush(stdout);
+      progress++;
+    }
+    for (int i = 0; i < max_operators; i++)
+    {
+      operator_digit = max_operators;
+      for (int j = 0; j < meg_number.size()-1; j++)
+      {
+        operators.at(j) = operator_characters.at((i%operator_digit)/(operator_digit/4));
+        operator_digit /= 4;
+      }
+      for (auto brackets : all_brackets)
+      {
+        meg_formula = make_formula(meg_number, operators, brackets, left_brackets_index, right_brackets_index);
+        if (std::fabs(calc_formula(meg_formula, 0, dummy) - number) < DELTA)
+        {
+          can_get_meg_formula = true;
+          brackets.at(4) = 0;
+          meg_formula = make_formula(meg_number, operators, brackets, left_brackets_index, right_brackets_index);
+          break;
+        }
+      }
+      if (can_get_meg_formula) break;
+    }
+    if (can_get_meg_formula) break;
+    count++;
+  } while (std::next_permutation(meg_number.begin(), meg_number.end()));
+  return can_get_meg_formula;
+}
+
+std::vector<std::vector<int> > get_all_brackets(std::vector<std::vector<int> > &left_brackets_index, std::vector<std::vector<int> > &right_brackets_index)
+{
+  /*
+  each bracket:
+  0 = (01)2345, 1 = (012)345, ... , 14 = 0123(45)
+  */
+  std::vector<std::pair<int, std::vector<int> > > tmp;
+  std::vector<std::vector<int> > ans;
+  std::vector<int> brackets(15, 0);
+  std::vector<std::pair<int, int> > brackets_pairs(15);
+  int max_brakects, bracket_digit, count;
+
+  for (int i = 0; i < 6; i++)
+  {
+    for (auto left : left_brackets_index.at(i))
+    {
+      brackets_pairs.at(left).first = i;
+    }
+    for (auto right : right_brackets_index.at(i))
+    {
+      brackets_pairs.at(right).second = i;
+    }
+  }
+  max_brakects = fast_pow(2, 15);
+  for (int i = 0; i < max_brakects; i++)
+  {
+    bracket_digit = max_brakects;
+    count = 0;
+    for (int j = 0; j < 15; j++)
+    {
+      brackets.at(j) = (i%bracket_digit)/(bracket_digit/2);
+      bracket_digit /= 2;
+      if (brackets.at(j)) count++;
+    }
+    if (!brackets.at(4)) continue;
+    tmp.push_back(std::make_pair(count, brackets));
+  }
+  std::sort(tmp.begin(), tmp.end());
+  for (int i = 0; i < tmp.size(); i++)
+  {
+    if (is_valid_brackets(tmp.at(i).second, brackets_pairs)) ans.push_back(tmp.at(i).second);
+  }
+  return ans;
+}
+
+bool is_valid_brackets(std::vector<int> &brackets, std::vector<std::pair<int, int> > &brackets_pairs)
+{
+  bool ans;
+  std::pair<int, int> bracket1, bracket2;
+
+  ans = true;
+  for (int i = 0; i < 14; i++)
+  {
+    if (!brackets.at(i)) continue;
+    bracket1 = brackets_pairs.at(i);
+    for (int j = i+1; j < 15; j++)
+    {
+      if (!brackets.at(j)) continue;
+      bracket2 = brackets_pairs.at(j);
+      if (bracket1.first < bracket2.first && bracket2.first <= bracket1.second && bracket1.second < bracket2.second)
+      {
+        ans = false;
+        break;
+      }
+      else if (bracket2.first < bracket1.first && bracket1.first <= bracket2.second && bracket2.second < bracket1.second)
+      {
+        ans = false;
+        break;
+      }
+    }
+    if (!ans) break;
+  }
+  return ans;
+}
+
+std::string make_formula(std::vector<int> &meg_number, std::vector<char> &operators, std::vector<int> &brackets,
+                         std::vector<std::vector<int> > &left_brackets_index, std::vector<std::vector<int> > &right_brackets_index)
+{
+  /*
+  each bracket:
+  0 = (01)2345, 1 = (012)345, ... , 14 = 0123(45)
+  */
+  std::string ans;
+
+  for (auto left : left_brackets_index.at(0))
+  {
+    if (brackets.at(left)) ans.push_back('(');
+  }
+  if (meg_number.at(0) < 10) ans.push_back('0' + meg_number.at(0));
+  else
+  {
+    ans.push_back('0' + meg_number.at(0)/10);
+    ans.push_back('0' + meg_number.at(0)%10);
+  }
+  for (int i = 0; i < 5; i++)
+  {
+    for (auto right : right_brackets_index.at(i))
+    {
+      if (brackets.at(right)) ans.push_back(')');
+    }
+    ans += operators.at(i);
+    for (auto left : left_brackets_index.at(i+1))
+    {
+      if (brackets.at(left)) ans.push_back('(');
+    }
+    if (meg_number.at(i+1) < 10) ans.push_back('0' + meg_number.at(i+1));
+    else
+    {
+      ans.push_back('0' + meg_number.at(i+1)/10);
+      ans.push_back('0' + meg_number.at(i+1)%10);
+    }
+  }
+  for (auto right : right_brackets_index.at(5))
+  {
+    if (brackets.at(right)) ans.push_back(')');
+  }
+  return ans;
+}
+
+int fast_pow(int x, int n)
+{
+  int ans = 1;
+  while (n)
+  {
+    if (n % 2 == 1)
+    {
+      ans *= x;
+      n--;
+    }
+    else
+    {
+      x *= x;
+      ans *= x;
+      n /= 2;
+      n--;
+    }
+  }
+  return ans;
+}
+
+/* ++++++++++ calculator ++++++++++ */
+double calc_formula(std::string meg_formula, int now, int &next)
+{
+  double left, right, right2;
+  int count, tmp, next2;
+  bool init_flag;
+
+  count = 0;
+  while (meg_formula[now] == ')' || meg_formula[now] == '+' || meg_formula[now] == '-' ||
+         meg_formula[now] == '*' || meg_formula[now] == '/') now++;
+  if (meg_formula[now] != '(')
+  {
+    int number = 0;
+    number += meg_formula[now] - '0';
+    if (now+1 < meg_formula.size() && meg_formula[now+1] >= '0' && meg_formula[now+1] <= '9')
+    {
+      now++;
+      number *= 10;
+      number += meg_formula[now] - '0';
+    }
+    if (now+1 == meg_formula.size()) next = -1;
+    else next = now+1;
+    return (double)number;
+  }
+  init_flag = true;
+  while (now != -1)
+  {
+    if (meg_formula[now] == '(' || meg_formula[now] == ')')
+    {
+      if (meg_formula[now] == '(') count++;
+      else count--;
+      now++;
+    }
+    if (!count)
+    {
+      next++;
+      break;
+
+    }
+    if (init_flag)
+    {
+      left = calc_formula(meg_formula, now, tmp);
+      now = tmp;
+      init_flag = false;
+    }
+    right = calc_formula(meg_formula, now, next);
+    while (next != -1 && (meg_formula[next] == '*' || meg_formula[next] == '/'))
+    {
+      right2 = calc_formula(meg_formula, next, next2);
+      right = calc(right, meg_formula[next], right2);
+      next = next2;
+    }
+    while (meg_formula[now] == ')') now++;
+    left = calc(left, meg_formula[now], right);
+    now = next;
+   }
+  return left;
+}
+
+double calc(double left, char op, double right)
+{
+  if (op == '+') return left + right;
+  else if (op == '-') return left - right;
+  else if (op == '*') return left * right;
+  else if (right != 0) return left / right;
+  else return PI;
+}
